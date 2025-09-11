5d2353caa618ffb0c2f163f64ff2d1c9e8b1ba00
YaelAlejandroUrbanoZuniga <124051558@upq.edu.mx>
2025-09-10 18:32:42 -0600

    1. Tipos de Datos

---
+++ JSParaPrincipiantes/app.js
@@ -0,0 +1,38 @@
+//---------------------TIPOS DE DATOS
+
+//string
+"Hola"
+'Hola'
+
+//number
+8
+-8
+
+//boolean
+true
+false
+
+//array
+['Fidel', 'Abdiel', 'Yael']
+[1,2,3]
+[true, false, true, true]
+
+//object
+'Fidel'
+70.4
+14
+true
+
+{
+    "username": 'Fidel',
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true
+};
+
+{
+    "username": 'Abdiel',
+    "score": 0.4,
+    "hours": 1,
+    "proffesional": false
+};
