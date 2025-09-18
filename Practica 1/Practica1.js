016681e783bde01491e7e3d773f27508e28cc396
YaelAlejandroUrbanoZuniga <124051558@upq.edu.mx>
2025-09-17 19:07:58 -0600

    Ejercicio b) - Practica 1
    Cmabiar una función tradicional a una de flecha.

--- Practica 1/Practica1.js
+++ Practica 1/Practica1.js
@@ -5,3 +5,17 @@ let edad = 25;
 let saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
 
 console.log(saludo);
+
+//-------------------------------------------------------------------------------
+
+const cuadrado = (numero) => numero * numero;
+let resultado1 = cuadrado(10)
+console.log(resultado1);
+
+let resultado2 = cuadrado(8)
+console.log(resultado2);
+
+let resultado3 = cuadrado(50)
+console.log(resultado3);
+
+//-------------------------------------------------------------------------------
