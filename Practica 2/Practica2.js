604df2198a91f0aa3f3934bcae5e4bf5ed98e5fe
YaelAlejandroUrbanoZuniga <124051558@upq.edu.mx>
2025-09-19 15:50:05 -0600

    Ejercicio b) - Practica 2
    Según el objeto productos, filtrar los precios mayores a 1000 y mostrar los nombres que cumplan con la condición.

--- Practica 2/Practica2.js
+++ Practica 2/Practica2.js
@@ -13,3 +13,20 @@ const persona =
 
 const {nombre, edad, direccion: {ciudad}} = persona;
 console.log("Me llamo " + nombre + ", tengo " + edad + " años y vivo en " + ciudad)
+
+// ------------------------------------EJERCICIO 2--------------------------------------------
+
+const productos = 
+[
+    {nombre: "Laptop", precio: 12000},
+    {nombre: "Mouse", precio: 250},
+    {nombre: "Teclado", precio: 750},
+    {nombre: "Monitor", precio: 3000},
+];
+
+let nombres = productos
+    .filter(productos => productos.precio > 1000)
+    .map(productos => productos.nombre)
+
+console.log(nombres);
+
