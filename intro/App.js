5b74d41a491c61d0061b0273b8f014f584158395
YaelAlejandroUrbanoZuniga <124051558@upq.edu.mx>
2025-10-10 15:58:36 -0600

    PRÁCTICA 6 - STYLESHEET & FLEX
    Práctica donde dimos estilo a nuestra proyecto anterior.

--- intro/App.js
+++ intro/App.js
@@ -1,32 +1,13 @@
 //1. IMPORTS: Zona de Importaciones----------------------------------------------------
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, {useState} from 'react';
+import ContadorScreen from './screens/ContadorScreen.js';
 
 //2. MAIN: Zona de Componentes---------------------------------------------------------
-export default function App() {
-  const[contador, setContador] = useState(0);
+export default function App() 
+{
+  
   return (
-    <View style={styles.container}>
-      <Text>Contador: {contador} </Text>
-      <Button title= "Agregar" onPress={()=>setContador(contador + 1)}></Button>
-
-      <Button title= "Quitar" onPress={()=>setContador(contador - 1)}></Button>
-
-      <Button title= "Reiniciar" onPress={()=>setContador(0)}></Button>
-
-      <StatusBar style="auto" />
-    </View>
+    <ContadorScreen></ContadorScreen>
   );
 }
 
 //3. STYLES: Zona de Estetica y Posicionamiento----------------------------------------
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
