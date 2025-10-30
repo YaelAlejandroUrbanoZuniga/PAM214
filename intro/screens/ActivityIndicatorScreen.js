3bf6038c8c0066b5dcb00972afd3c63c833397fe
YaelAlejandroUrbanoZuniga <124051558@upq.edu.mx>
2025-10-30 17:22:30 -0600

    ActivityIndicator: Completada
    Práctica 13: ActivityIndicator Completada

--- intro/screens/ActivityIndicatorScreen.js
+++ intro/screens/ActivityIndicatorScreen.js
@@ -1,12 +1,59 @@
-import { Text, StyleSheet, View, Button } from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, Button, ActivityIndicator, StyleSheet } from 'react-native';
 
-export default function ActivityIndicator() 
+export default function ActivityScreen() 
 {
-    return(
-        <View>
-            <Text>Proximamente by: Israel Esau</Text>
+    const [cargando, setCargando] = useState(false);
+
+    const iniciarCarga = () => 
+    {
+        setCargando(true);
+        setTimeout(() => setCargando(false), 3000);
+    };
+
+    return (
+    <View style={styles.container}>
+        <Text style={styles.texto}>Presiona para iniciar carga</Text>
+
+        <ActivityIndicator
+            style={styles.indicador}
+            animating={cargando}
+            size="large"
+            color="#007BFF"
+            hidesWhenStopped={true}
+        />
+
+        <View style={styles.botonContainer}>
+            <Button color="red" title="Iniciar carga" onPress={iniciarCarga}/>
         </View>
-    )
+    </View>
+    );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create(
+{
+    container: 
+    {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F8F9FA',
+    },
+    texto: 
+    {
+        fontFamily: 'Times New Roman',
+        marginBottom: 20,
+        fontSize: 18,
+        color: '#333',
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+    },
+    indicador: 
+    {
+        marginVertical: 10,
+    },
+    botonContainer: 
+    {
+        marginTop:30,
+    },
+});
